b37be20ff841abb7b676025d4d9d57d382beeded
Rawand Dargali <rdargali@yahoo.com>
2020-04-23 02:19:59 -0500

    stop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const allPlayers = `https://www.balldontlie.io/api/v1/players?page=2`;
+
+  useEffect(() => {
+    axios.get(allPlayers).then((res) => console.log(res.data));
+  }, []);
   return (
     <div className="App">
       <h1>whatup</h1>
     </div>
   );
-}
+};
 
 export default App;
