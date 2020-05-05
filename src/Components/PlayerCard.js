11cba7a5ed95381e9f487fd939759058b727ad03
Rawand Dargali <rdargali@yahoo.com>
2020-05-05 18:32:07 -0500

    player card condensed and  working with tooltip

--- src/Components/PlayerCard.js
+++ src/Components/PlayerCard.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "../style/PlayerCard.css";
 
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+
 const PlayerCard = ({ players, removePlayer }) => {
   const playerCardJSX = players.map((player) => (
-    <div key={player.id} className="card">
-      <i
-        className="far fa-times-circle delete-icon"
-        onClick={() => removePlayer(player.id)}
-      />
-      <div className="container">
-        <strong>
-          {player.first_name} {player.last_name}
-        </strong>
-
-        <br />
-        <small>
-          {player.team.full_name} - {player.position}
-        </small>
+    <Tippy content={`${player.team.full_name} - ${player.position}`}>
+      <div key={player.id} className="card">
+        <i
+          className="far fa-times-circle delete-icon"
+          onClick={() => removePlayer(player.id)}
+        />
+        <div className="container">
+          <small>
+            {player.first_name} {player.last_name}
+          </small>
+        </div>
       </div>
-    </div>
+    </Tippy>
   ));
 
   return <div className="player-card-container">{playerCardJSX}</div>;
